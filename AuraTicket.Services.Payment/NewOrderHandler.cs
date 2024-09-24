046f6710bf66a2a1195de7e17e319c126b31201c
Omer Farook <49291618+omerfarookbe@users.noreply.github.com>
2024-09-23 20:17:44 -0400

    payment project

---
+++ AuraTicket.Services.Payment/NewOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using AuraTicket.Messages;
+using Rebus.Handlers;
+
+namespace AuraTicket.Services.Payment
+{
+    public class NewOrderHandler : IHandleMessages<PaymentRequestMessage>
+    {
+        public Task Handle(PaymentRequestMessage message)
+        {
+            Console.WriteLine($"Payment request received for basket id {message.BasketId}.");
+            return Task.CompletedTask;
+        }
+    }
+}
